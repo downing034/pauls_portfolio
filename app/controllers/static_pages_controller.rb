5066f4074bd8ed1f34fb3b681cea66a84543cb32
Paul Downing <downing034@gmail.com>
2015-08-09 21:45:11 -0500

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def completed_apps
+  end
+
+  def in_progress_apps
+  end
+
+  def resume
+  end
+end
